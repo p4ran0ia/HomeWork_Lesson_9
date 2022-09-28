3ead6d2f2d909e3d623ca5a7b884dda59a9b7a59
Alexandr <crazy.top@mail.ru>
2022-09-28 17:14:02 +0300

    complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int Summa(int m, int n)
+{
+    if (m == n)
+        return n;
+    return Summa(m + 1, n) + m;
+}
+
+Console.Write("Введите минимальное значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Summa(m, n));
+
+
+
---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int AkkermanFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return AkkermanFunc(m - 1, 1);
+    }
+    else
+    {
+        return (AkkermanFunc(m - 1, AkkermanFunc(m, n - 1)));
+    }
+}
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AkkermanFunc(m, n));
